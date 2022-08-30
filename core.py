79aaea361249f1da55124161a2d2603e451d14dd
Safa Messaoud <safamesssaoud@gmail.com>
2022-08-30 11:14:00 +0300

    safa

--- core.py
+++ core.py
@@ -178,7 +178,7 @@ class MaxEntrRL():
             
             while not(d or (ep_len == self.RL_kwargs.max_ep_len)):
                 # Take deterministic actions at test time 
-                a = self.ac(np.expand_dims(o, axis=0), deterministic=self.ac.pi.test_deterministic, test=True)
+                a = self.ac(np.expand_dims(o, axis=0), deterministic=self.ac.pi.test_deterministic, with_logprob=False)
                 o, r, d, _ = self.test_env.step(a)
                 ep_ret += r
                 ep_len += 1
