27a135df29818078d21657ac4014a62999057879
Billel Mokeddem <bilel.crysis3@gmail.com>
2022-09-04 15:55:50 +0300

    Removing the kernel from this file

--- STAC/actors/actor_svgd.py
+++ STAC/actors/actor_svgd.py
@@ -5,49 +5,7 @@ import torch.nn.functional as F
 import torch.autograd as autograd
 from networks import MLPSquashedGaussian
 from torch.distributions import Normal, Categorical
-from networks import mlp
-
-class RBF(torch.nn.Module):
-    def __init__(self, parametrized=False, act_dim=None, hidden_sizes=None, activation=torch.nn.ReLU, num_particles=None):
-        super(RBF, self).__init__()
-        self.parametrized = parametrized
-        self.num_particles = num_particles
-
-        if parametrized:
-            self.log_std_layer = mlp([num_particles*num_particles] + list(hidden_sizes) + [act_dim] , activation)
-            self.log_std_min = 2
-            self.log_std_max = -20
-            
-
-    def forward(self, input_1, input_2,  h_min=1e-3):
-        _, out_dim1 = input_1.size()[-2:]
-        _, out_dim2 = input_2.size()[-2:]
-        num_particles = input_2.size()[-2]
-        assert out_dim1 == out_dim2
-        
-        # Compute the pairwise distances of left and right particles.
-        diff = input_1.unsqueeze(-2) - input_2.unsqueeze(-3)
-        dist_sq = diff.pow(2).sum(-1)
-        dist_sq = dist_sq.unsqueeze(-1)
-        
-        if self.parametrized == False:
-            # Get median.
-            median_sq = torch.median(dist_sq.detach().reshape(-1, num_particles*num_particles), dim=1)[0]
-            median_sq = median_sq.reshape(-1,1,1,1)
-            h = median_sq / (2 * np.log(num_particles + 1.))
-            sigma = torch.sqrt(h)
-        else:
-            log_std = self.log_std_layer(dist_sq)
-            log_std = torch.clamp(log_std, self.log_std_min, self.log_std_max)
-            sigma = torch.exp(log_std)
-        
-        gamma = 1.0 / (1e-8 + 2 * sigma**2) 
-        kappa = (-gamma * dist_sq).exp()
-        kappa_grad = -2. * (diff * gamma) * kappa
-        return kappa.squeeze(-1), diff, gamma, kappa_grad
-
-
-
+from actors.kernels import RBF
 
 class ActorSvgd(torch.nn.Module):
     def __init__(self, actor, obs_dim, act_dim, act_limit, num_svgd_particles, num_svgd_steps, svgd_lr, test_deterministic, batch_size, device, hidden_sizes, q1, q2, activation=torch.nn.ReLU):
@@ -135,7 +93,7 @@ class ActorSvgd(torch.nn.Module):
         a = a.view(-1, self.num_particles, self.act_dim)
         # at test time
         if (with_logprob == False) and (deterministic == True):
-            a = torch.gather(a, 1, torch.max(q_s_a, dim=1)[1].unsqueeze(-1))
+            a = a[:,torch.max(q_s_a, dim=1)[1]]
          
         elif (with_logprob == False) and (deterministic == False):
             beta = 1
@@ -144,7 +102,7 @@ class ActorSvgd(torch.nn.Module):
 
             nominator = torch.exp(beta * q_s_a - max_q_value) # (-1, np)
             denominator = torch.sum(nominator, dim=1, keepdim=True) # (-1, 1)
-            #denominator = denominator.repeat((-1, self.num_particles))  # (-1, np)
+            # denominator = denominator.repeat((-1, self.num_particles))  # (-1, np)
 
             soft_max_porbs = (nominator / denominator) # (-1, np)
             dist = Categorical(soft_max_porbs)
