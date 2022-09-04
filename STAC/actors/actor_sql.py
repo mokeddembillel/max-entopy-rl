eb9bb9f4ef60cf5a4ae12144b59c05a184bb957c
Billel Mokeddem <bilel.crysis3@gmail.com>
2022-09-04 15:57:27 +0300

    Fixing bugs in SQL

--- STAC/actors/actor_sql.py
+++ STAC/actors/actor_sql.py
@@ -2,9 +2,12 @@ import torch
 import torch.nn as nn
 from networks import mlp
 from torch.distributions import Categorical
+from actors.kernels import RBF
+
 
 class ActorSql(nn.Module):
     def __init__(self, actor_name, obs_dim, act_dim, act_limit, num_svgd_particles, svgd_lr, test_deterministic, batch_size, device, hidden_sizes, q1, q2, activation=nn.ReLU):
+        super(ActorSql, self).__init__()
         self.actor_name = actor_name
         self.obs_dim = obs_dim
         self.act_dim = act_dim
@@ -15,9 +18,11 @@ class ActorSql(nn.Module):
         
         self.q1 = q1
         self.q2 = q2
-        self.concat = mlp([self.state_dim + self.action_dim] + list(hidden_sizes),activation)
-        self.layer2 =  mlp(list(hidden_sizes) + [self.action_dim], nn.Tanh, nn.Tanh)
+        self.concat = mlp([self.obs_dim + self.act_dim] + list(hidden_sizes),activation)
+        self.layer2 =  mlp(list(hidden_sizes) + [self.act_dim], nn.Tanh, nn.Tanh)
         
+        self.kernel = RBF()
+
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         self.to(self.device)
     
@@ -36,13 +41,13 @@ class ActorSql(nn.Module):
 
         q1_values = self.q1(state, action)
         q2_values = self.q2(state, action)
-        q_values = torch.min(torch.concat((q1_values, q2_values), dim=-1),dim=-1)
+        q_values = torch.min(q1_values, q2_values)
 
         action = action.view(-1, self.num_particles, self.act_dim) # (-1, np, ad)
         q_values = q_values.view(-1, self.num_particles) # (-1, np)
 
         if (with_logprob == False) and (deterministic == True):
-            action = torch.gather(action, 1, torch.max(q_values, dim=1)[1].unsqueeze(-1))
+            action = action[:,torch.max(q_values, dim=1)[1]]
          
         elif (with_logprob == False) and (deterministic == False):
             beta = 1
@@ -51,9 +56,11 @@ class ActorSql(nn.Module):
 
             nominator = torch.exp(beta * q_values - max_q_value) # (-1, np)
             denominator = torch.sum(nominator, dim=1, keepdim=True) # (-1, 1)
-            denominator = denominator.repeat((-1, self.num_particles))  # (-1, np)
+            # denominator = denominator.repeat((-1, self.num_particles))  # (-1, np)
 
             soft_max_porbs = (nominator / denominator) # (-1, np)
             dist = Categorical(soft_max_porbs)
             action = torch.gather(q_values, 1, dist.sample().unsqueeze(-1))
-        return action
+        else:
+            action = action.view(-1, self.act_dim)
+        return action, None
