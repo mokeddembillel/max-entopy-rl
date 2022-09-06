89a1dae35c6866b141f822a68ff33758c7a963bb
Billel Mokeddem <bilel.crysis3@gmail.com>
2022-09-06 03:27:21 +0300

    Cleaning and moving everything related to debugging to the debugging class.

--- STAC/envs/multigoal_env.py
+++ STAC/envs/multigoal_env.py
@@ -7,11 +7,6 @@ import torch
 import glob
 import os
 
-def gaussian(x, mu, sig):
-    out = np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
-    out = np.tanh(out)
-    return out 
-
 class PointDynamics(object):
     """
     State: position.
@@ -37,7 +32,7 @@ class MultiGoalEnv(Env, EzPickle):
     State: position.
     Action: velocity.
     """
-    def __init__(self, writer=None, actor=None, goal_reward=10, actuation_cost_coeff=30.0,distance_cost_coeff=1.0,
+    def __init__(self, writer=None, actor=None, fig_path=None, goal_reward=10, actuation_cost_coeff=30.0,distance_cost_coeff=1.0,
                  init_sigma=0.05, max_steps=30):
         EzPickle.__init__(**locals())
 
@@ -63,9 +58,9 @@ class MultiGoalEnv(Env, EzPickle):
         self.writer = writer
         self.actor = actor
         self.episodes_information = []
-
+        self.fig_path = fig_path
         # remove the content of the fig folder
-        figs = glob.glob('./STAC/multi_goal_plots_/*')
+        figs = glob.glob(self.fig_path + '*')
         [os.remove(fig) for fig in figs]
     
     def reset(self, init_state=None):
@@ -75,17 +70,13 @@ class MultiGoalEnv(Env, EzPickle):
             unclipped_observation = (self.init_mu + self.init_sigma * np.random.normal(size=self.dynamics.s_dim))
 
         self.observation = np.clip(unclipped_observation, self.observation_space.low, self.observation_space.high)
-        
-        self.episodes_information.append({'observations':[self.observation],
-                            'actions': [],
-                            'rewards': [],
-                            'status': None,
-                            'goal': None, 
-                            'mu': [],
-                            'sigma': [],
-                            'q_hess' : [],
-                            'q_score': [],
-                            })
+        self.episodes_information.append({
+            'observations':[self.observation],
+            # 'actions': [],
+            # 'rewards': [],
+            # 'status': None,
+            # 'goal': None, 
+            })
         
         return self.observation
 
@@ -110,7 +101,7 @@ class MultiGoalEnv(Env, EzPickle):
         # compute action
         action = action.ravel()
         action = np.clip(action, self.action_space.low, self.action_space.high).ravel()
-        self.episodes_information[-1]['actions'].append(action)
+        # self.episodes_information[-1]['actions'].append(action)
 
         # compute observation
         observation = self.dynamics.forward(self.observation, action)
@@ -121,18 +112,28 @@ class MultiGoalEnv(Env, EzPickle):
         reward = self.compute_reward(observation, action)
 
         # compute done
-        dist_to_goal = np.amin([np.linalg.norm(observation - goal_position)for goal_position in self.goal_positions])
-        done = dist_to_goal < self.goal_threshold
+        distance_to_goals = [np.linalg.norm(observation - goal_position)for goal_position in self.goal_positions]
+        min_dist_index = np.argmin(distance_to_goals)
+        done = distance_to_goals[min_dist_index] < self.goal_threshold
+        
         
         # reward at the goal
         if done:
+            goal = np.zeros((self.num_goals,))[min_dist_index] = 1.
+            status = 'succeded'
             reward += self.goal_reward
-        
-        self.episodes_information[-1]['rewards'].append(reward)
+
+        else:
+            if len(self.episode_information[-1]['observations']) == self.max_steps - 1:
+                done = True
+                status = 'Failed'
+                goal = np.zeros((self.num_goals,))
+            
+        # self.episodes_information[-1]['rewards'].append(reward)
 
         self.observation = np.copy(observation)
 
-        return observation, reward, done, {'pos': observation}
+        return observation, reward, done, {'goal': goal, 'status': status}
     
 
     def compute_reward(self, observation, action): 
@@ -211,188 +212,14 @@ class MultiGoalEnv(Env, EzPickle):
         self._env_lines = []
         
         # compute the number of runs reaching the goals
-        number_of_hits_mode = np.zeros(self.num_goals)
-        q_score, q_score_min, q_score_max = [], [], []
-        q_hess, q_hess_min, q_hess_max = [], [], []
-        
         for _, path in enumerate(self.episodes_information):
-            
             positions = np.stack(path['observations'])
-
             # self._env_lines += self._ax.plot(positions[-1, 0], positions[-1, 1], '+b)
             self._env_lines += self._ax.plot(positions[:, 0], positions[:, 1])
-            #compute the number of modes
-            modes_dist = ((positions[-1].reshape(-1,2)-self.goal_positions)**2).sum(-1)
 
-            if modes_dist.min()<1:
-                number_of_hits_mode[modes_dist.argmin()]+=1 
-            
-            ###
-            q_score_ = np.stack(path['q_score'])
-            q_score.append(q_score_.mean())
-            q_score_min.append(q_score_.min())
-            q_score_max.append(q_score_.max())
-
-            q_hess_ = np.stack(path['q_hess'])
-            q_hess.append(q_hess_.mean())
-            q_hess_min.append(q_hess_.min())
-            q_hess_max.append(q_hess_.max())
-        
-        
-        plt.savefig('./STAC/multi_goal_plots_/'+ str(itr)+".pdf")   
+        plt.savefig(self.fig_path+ str(itr)+".pdf")   
         plt.close()
 
-        # logging
-        #  log the number of hits across episodes for each of the modes
-        self.writer.add_scalar('modes/num_modes',(number_of_hits_mode>0).sum(), itr)
-        self.writer.add_scalar('modes/total_number_of_hits_mode',number_of_hits_mode.sum(), itr)
-        
-        for ind in range(self.num_goals):
-            self.writer.add_scalar('modes/prob_mod_'+str(ind),number_of_hits_mode[ind]/number_of_hits_mode.sum(), itr)
-        
-        self.writer.add_scalars('smoothness/q_score',  {'Mean ': np.mean(q_score), 'Min': np.mean(q_score_min), 'Max': np.mean(q_score_max)  }, itr)
-        self.writer.add_scalars('smoothness/q_hess', {'Mean ': np.mean(q_hess), 'Min': np.mean(q_hess_min), 'Max': np.mean(q_hess_max)  }, itr)
-        
 
         
-    def plot_policy(self, itr):
-        """Rendering the past rollouts of the environment.""" 
-        
-        self._init_plot()
-        # noinspection PyArgumentList
-        [line.remove() for line in self._env_lines]
-        self._env_lines = []
-        
-        path = self.episodes_information[0]
-            
-        positions = np.stack(path['observations'])
-
-        self._env_lines += self._ax.plot(positions[:, 0], positions[:, 1], '+b')
-
-        for i in range(len(positions)):
-            self._ax.annotate(str(i), (positions[i,0], positions[i,1]), fontsize=6)
-
-        for i in range(len(positions)-1):
-
-            if self.actor in ['sac', 'svgd_p0_pram', 'svgd_p0_kernel_pram']:
-                mu = path['mu'][i][0]
-                std = path['sigma'][i][0]
-            else:
-                mu = 0
-                std = 1
-
-            x_values = np.linspace(positions[i]+mu+self.action_space.low, positions[i]+mu+self.action_space.high , 30) 
-            plt.plot(x_values[:,0] , gaussian(x_values, positions[i]+mu, std)[:,0] )
-
-        plt.savefig('./STAC/multi_goal_plots_/path_vis_'+ str(itr)+".pdf")   
-        plt.close()
-            
-    
-    def collect_data_for_logging(self, ac, o, a):
-
-        if self.actor in ['sac', 'svgd_p0_pram', 'svgd_p0_kernel_pram']:
-            self.episodes_information[-1]['mu'].append(ac.pi.mu.detach().cpu().numpy())
-            self.episodes_information[-1]['sigma'].append(ac.pi.sigma.detach().cpu().numpy())
-        
-
-# <<<<<<< HEAD
-#         if self.actor in  ['svgd_nonparam', 'svgd_p0_pram', 'svgd_p0_kernel_pram']:
-#             self.episodes_information[-1]['q_hess'].append(ac.pi.hess_list)
-#             self.episodes_information[-1]['q_score'].append(ac.pi.score_func_list)
-#             self.episodes_information[-1]['q_hess_eig_max'].append(ac.pi.hess_eig_max)
-#             self.episodes_information[-1]['q_particles_var'].append(ac.pi.hess_eig_max)
-        
-        if self.actor in  ['sac', 'svgd_nonparam', 'svgd_p0_pram', 'svgd_p0_kernel_pram', 'svgd_sql']:
-            # print('############ ', o.shape, a.shape)
-            grad_q_ = torch.autograd.grad(ac.q1(o,a), a, retain_graph=True, create_graph=True)[0].squeeze()
-            hess_q = ((torch.abs(torch.autograd.grad(grad_q_[0], a, retain_graph=True)[0])+torch.abs(torch.autograd.grad(grad_q_[1], a, retain_graph=True)[0])).sum()/4)
-            self.episodes_information[-1]['q_score'].append(torch.abs(grad_q_).mean().detach().cpu().numpy())
-            self.episodes_information[-1]['q_hess'].append(hess_q.detach().cpu().numpy())
-        
-    
-    def debugging_metrics(self, itr, ac, num_svgd_particles):
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        init_state = torch.tensor([0.0,0.0]).to(device)
-        a_up = torch.tensor([0.0,0.7]).to(device)
-        a_down = torch.tensor([0.0,-0.7]).to(device)
-        a_left = torch.tensor([-0.7,0.0]).to(device)
-        a_right = torch.tensor([0.7,0.0]).to(device)
-
-        q_up = ac.q1(init_state,a_up).detach()
-        q_down = ac.q1(init_state,a_down).detach()
-        q_left = ac.q1(init_state,a_left).detach()
-        q_right = ac.q1(init_state,a_right).detach()
-        self.writer.add_scalars('init_state/q_val',{'q_up': q_up, 'q_down':q_down, 'q_left':q_left, 'q_right':q_right}, itr)
-
-        def compute_q(init_s, action):
-            return ac.q1(init_s,action)
-
-
-        # add curvature and gradient
-        a_up_ = a_up.requires_grad_(True)
-        init_state_ = init_state.requires_grad_(True)
-        grad_up_ = torch.autograd.grad(ac.q1(init_state_,a_up_), a_up_,retain_graph=True, create_graph=True)[0]
-        grad_up = torch.abs(grad_up_).mean()
-        hess_up = ((torch.abs(torch.autograd.grad(grad_up_[0],a_up_,retain_graph=True)[0])+torch.abs(torch.autograd.grad(grad_up_[1],a_up_,retain_graph=True)[0])).sum()/4).detach()
-        #hess_up = torch.autograd.functional.hessian(compute_q, (init_state_,a_up_) )
-        #hess_up = torch.abs(hess_up[1][1]).mean().detach()
-        ###
-        a_down_ = a_down.requires_grad_(True)
-        grad_down_ = torch.autograd.grad(ac.q1(init_state_,a_down_), a_down_,retain_graph=True, create_graph=True)[0]
-        grad_down = torch.abs(grad_down_).mean()
-        hess_down = ((torch.abs(torch.autograd.grad(grad_down_[0],a_down_,retain_graph=True)[0])+torch.abs(torch.autograd.grad(grad_down_[1],a_down_,retain_graph=True)[0])).sum()/4).detach()
-        #hess_down = torch.autograd.functional.hessian(compute_q, (init_state_,a_down_) )
-        #hess_down = torch.abs(hess_down[1][1]).mean().detach()
-        ###
-        a_left_ = a_left.requires_grad_(True)
-        grad_left_ = torch.autograd.grad(ac.q1(init_state_,a_left_), a_left_,retain_graph=True, create_graph=True)[0]
-        grad_left = torch.abs(grad_left_).mean()
-        hess_left = ((torch.abs(torch.autograd.grad(grad_left_[0],a_left_,retain_graph=True)[0])+torch.abs(torch.autograd.grad(grad_left_[1],a_left_,retain_graph=True)[0])).sum()/4).detach()
-        #hess_left = torch.autograd.functional.hessian(compute_q, (init_state_,a_left_) )
-        #hess_left = torch.abs(hess_left[1][1]).mean().detach()
-        ###
-        a_right_ = a_right.requires_grad_(True)
-        grad_right_ = torch.autograd.grad(ac.q1(init_state_,a_right_), a_right_,retain_graph=True, create_graph=True)[0]
-        grad_right = torch.abs(grad_right_).mean()
-        hess_right = ((torch.abs(torch.autograd.grad(grad_right_[0],a_right_,retain_graph=True)[0])+torch.abs(torch.autograd.grad(grad_right_[1],a_right_,retain_graph=True)[0])).sum()/4).detach()
-        #hess_right = torch.autograd.functional.hessian(compute_q, (init_state_,a_right_) )
-        #hess_right = torch.abs(hess_right[1][1]).mean().detach()
-        
-        #import pdb; pdb.set_trace()  
-        self.writer.add_scalars('init_state/hessian',{'hess_up': hess_up, 'hess_down':hess_down, 'hess_left':hess_left, 'hess_right':hess_right}, itr)
-        self.writer.add_scalars('init_state/grad',{'grad_up': grad_up, 'grad_down':grad_down, 'grad_left':grad_left, 'grad_right':grad_right}, itr)
-        
-        
-        # compute the variance of running svgd
-        num_samples = 100
-
-        s_up = a_up.view(-1,1,a_up.size()[-1]).repeat(1,num_samples*num_svgd_particles,1).view(-1,a_up.size()[-1])
-        a_rand = torch.normal(0, 1, size=(len(s_up),2)).to(device)
-        a_svgd_up, _, _ = ac.pi(s_up, a_rand.detach()) 
-        q_svgd_up = ac.q1(s_up,a_svgd_up).detach()
-        q_svgd_up_var = torch.var(q_svgd_up)
-        
-        s_down = a_down.view(-1,1,a_down.size()[-1]).repeat(1,num_samples*num_svgd_particles,1).view(-1,a_down.size()[-1])
-        a_rand = torch.normal(0, 1, size=(len(s_down),2)).to(device)
-        a_svgd_down, _, _ = ac.pi(s_down, a_rand.detach()) 
-        q_svgd_down = ac.q1(s_down,a_svgd_down).detach()
-        q_svgd_down_var = torch.var(q_svgd_down)
-        
-        s_left = a_left.view(-1,1,a_left.size()[-1]).repeat(1,num_samples*num_svgd_particles,1).view(-1,a_left.size()[-1])
-        a_rand = torch.normal(0, 1, size=(len(s_left),2)).to(device)
-        a_svgd_left, _, _ = ac.pi(s_left, a_rand.detach()) 
-        q_svgd_left = ac.q1(s_left,a_svgd_left).detach()
-        q_svgd_left_var = torch.var(q_svgd_left)
-        
-        s_right = a_left.view(-1,1,a_right.size()[-1]).repeat(1,num_samples*num_svgd_particles,1).view(-1,a_right.size()[-1])
-        a_rand = torch.normal(0, 1, size=(len(s_right),2)).to(device)
-        a_svgd_right, _, _ = ac.pi(s_right, a_rand.detach()) 
-        q_svgd_right = ac.q1(s_right,a_svgd_right).detach()
-        q_svgd_right_var = torch.var(q_svgd_right)
-
-        #if itr==2000:
-        #    import pdb; pdb.set_trace()
-        self.writer.add_scalars('init_state/q_var',{'q_up': q_svgd_up_var, 'q_down':q_svgd_down_var, 'q_left':q_svgd_left_var, 'q_right':q_svgd_right_var}, itr)
-
-
 
